1c0cb179e0b7eb2bafb3885119d4f6c86cde11fe
shdg37844 <missatail886@gmail.com>
2023-12-20 23:46:26 +0800

    Added admin directory

---
+++ admin/admin/src/router/routes.js
@@ -0,0 +1,56 @@
+import Article from "../../pages/Article.vue";
+import ArticleCreate from '../../pages/ArticleCreate.vue';
+import ArticleEdit from '../../pages/ArticleEdit.vue';
+import Classify from '../../pages/Classify.vue';
+import Login from '../../pages/Login.vue';
+import User from '../../pages/User.vue';
+import ManageLayout from '../../pages/ManageLayout.vue';
+
+const routes = [
+    {
+        path: "/admin/login",
+        name: "Login",
+        component: Login,
+
+    },
+    {
+        path: "/admin",
+        name: "manage",
+        component: ManageLayout,
+        redirect: '/admin/user',
+        children: [
+            {
+                path: "/user",
+                name: "user",
+                component: User,
+            },
+            {
+                path: "/classify",
+                name: "classify",
+                component: Classify,
+            },
+            {
+                path: "/article",
+                name: "article",
+                component: Article,
+                children: [
+                    {
+                        path: "/create",
+                        name: "create",
+                        component: ArticleCreate,
+                    },
+                    {
+                        path: "/edit",
+                        name: "edit",
+                        component: ArticleEdit,
+                    },
+                ]
+            },
+        ]
+    },
+
+
+
+];
+
+export default routes
